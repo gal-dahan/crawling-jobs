fda973fdf3e72def68793f6f2e88258175fe9e09
Gal-dahan <963gal963@gmail.com>
2022-09-19 23:01:24 +0300

    Update css

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -7,6 +7,7 @@
     <h1>About us</h1>
     <div className="about">
         <span> Our web application gathers job offers by scraping public websites using a Node script, every six hours and after two months automatically removes the data. We don't block content behind paywalls: you can browse all the jobs offer without any account and without signing-up.</span>
+        <span> <strong>We're developing in the open source <a href={"https://github.com/gal-dahan/crawling-jobs"} target="_blank" rel="noopener"  >Available on GitHub. </a></strong></span>
     </div>
     </div>
   )
