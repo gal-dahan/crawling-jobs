e136b36876df7859556dd5cafb04fa7a2b502163
Gal-dahan <963gal963@gmail.com>
2022-09-19 22:35:17 +0300

    About us

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -6,7 +6,7 @@
 }}> 
     <h1>About us</h1>
     <div className="about">
-        <p>We don't block content behind paywalls: you can browse all the jobs offer without any account and without signing-up.</p>
+        <span> Our web application gathers job offers by scraping public websites using a Node script, every six hours and after two months automatically removes the data. We don't block content behind paywalls: you can browse all the jobs offer without any account and without signing-up.</span>
     </div>
     </div>
   )
